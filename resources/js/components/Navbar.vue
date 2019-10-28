f7e857bcec8b774edc12b684c32ebaa97108aa91
Mart Kiner <kinermercurio@gmail.com>
2019-10-28 17:00:16 +0800

    v1 laravel vue crud

---
+++ resources/js/components/Navbar.vue
@@ -0,0 +1,7 @@
+<template>
+    <nav class="navbar navbar-expand-sm navbar-dar bg-info mb-2">
+        <div class="container">
+            <a href="#" class="navbar-brand">Laravel VUE</a>
+        </div>
+    </nav>
+</template>
